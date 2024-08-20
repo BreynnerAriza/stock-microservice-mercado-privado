e76b2199d7ff01e1b821f8adfbef38e101f30eeb
Breynner Ariza <breynner.fabian.ariza.florez@gmail.com>
2024-08-19 21:50:30 -0500

    Firts commit.

---
+++ src/main/java/com/stock/mercado/privado/mercado_privado/MercadoPrivadoApplication.java
@@ -0,0 +1,13 @@
+package com.stock.mercado.privado.mercado_privado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoPrivadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoPrivadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock/mercado/privado/mercado_privado/MercadoPrivadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.mercado.privado.mercado_privado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoPrivadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
